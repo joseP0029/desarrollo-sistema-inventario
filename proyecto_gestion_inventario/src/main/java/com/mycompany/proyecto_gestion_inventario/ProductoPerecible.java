5da30fde2d9aa28435a100650abe0f931a17329e
Jose P <josepinzon0029@gmail.com>
2024-10-16 10:12:12 -0500

    Creada la clase productoPerecible

---
+++ proyecto_gestion_inventario/src/main/java/com/mycompany/proyecto_gestion_inventario/ProductoPerecible.java
@@ -0,0 +1,22 @@
+package com.mycompany.proyecto_gestion_inventario;
+
+public class ProductoPerecible extends Producto{
+    private boolean refrigerado;
+
+    public ProductoPerecible() {
+    }
+
+    public ProductoPerecible(boolean refrigerado, int id, int cantidad, String nombre, String categoria, String nombreFabricante, String detallesEnvio, float precio, Proveedor proveedor) {
+        super(id, cantidad, nombre, categoria, nombreFabricante, detallesEnvio, precio, proveedor);
+        this.refrigerado = refrigerado;
+    }
+
+    public boolean isRefrigerado() {
+        return refrigerado;
+    }
+
+    public void setRefrigerado(boolean refrigerado) {
+        this.refrigerado = refrigerado;
+    }
+    
+}
