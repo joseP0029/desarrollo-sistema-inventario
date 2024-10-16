0d3632b1b2c4e4a8b9c5b54e5d6da0af87167d38
Jose P <josepinzon0029@gmail.com>
2024-10-16 08:24:49 -0500

    Creado el proyecto

---
+++ proyecto_gestion_inventario/src/main/java/com/mycompany/proyecto_gestion_inventario/Proyecto_gestion_inventario.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyecto_gestion_inventario;
+
+/**
+ *
+ * @author josep
+ */
+public class Proyecto_gestion_inventario {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
