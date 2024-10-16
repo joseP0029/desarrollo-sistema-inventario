449b0414bb5662fc4acdbb550e67da4f56ccb17a
Jose P <josepinzon0029@gmail.com>
2024-10-16 10:20:42 -0500

    Creada la clase productoNoPerecible

---
+++ proyecto_gestion_inventario/src/main/java/com/mycompany/proyecto_gestion_inventario/ProductoNoPerecible.java
@@ -0,0 +1,13 @@
+package com.mycompany.proyecto_gestion_inventario;
+
+public class ProductoNoPerecible extends Producto{
+
+    public ProductoNoPerecible() {
+    }
+
+    public ProductoNoPerecible(int id, int cantidad, String nombre, String categoria, String nombreFabricante, String detallesEnvio, float precio, Proveedor proveedor) {
+        super(id, cantidad, nombre, categoria, nombreFabricante, detallesEnvio, precio, proveedor);
+    }
+    
+    
+}
