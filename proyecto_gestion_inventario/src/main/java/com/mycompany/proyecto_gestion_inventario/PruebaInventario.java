3c7fe119682706ebcdf66e8c3bc8055bda74f69b
Jose P <josepinzon0029@gmail.com>
2024-11-11 16:07:21 -0500

    cambios

--- proyecto_gestion_inventario/src/main/java/com/mycompany/proyecto_gestion_inventario/PruebaInventario.java
+++ proyecto_gestion_inventario/src/main/java/com/mycompany/proyecto_gestion_inventario/PruebaInventario.java
@@ -4,7 +4,7 @@ public class PruebaInventario {
 
     public static void main(String[] args) {
         
-        Almacen almacenPrueba = new Almacen("ArchivoDePruebas.dat");
+        Almacen almacenPrueba = new Almacen("C:\\datos\\objetos.dat");
         almacenPrueba.leer();
         if (almacenPrueba.cuenta() == 0){
             Producto p = new Producto(1, 50, "Laptop Lenovo", "Electrónica", "Lenovo", "Envío gratuito a todo el país", 1200.99f, new Proveedor("Distribuciones ALFA", "Bucaramanga", "555-1234"));
@@ -18,10 +18,10 @@ public class PruebaInventario {
             p = new ProductoPerecible(true,5, 500, "Jugo de Naranja Natural", "Bebidas", "Natural Juice Co.", "Envío rápido con empaque reciclable", 1.99f, new Proveedor("Distribuciones ALFA", "Bucaramanga", "555-1234"));
             almacenPrueba.agregarProducto(p);
 
-            if (!almacenPrueba.guardar())
-            System.out.println("Error al guardar en el archivo");
+            if (almacenPrueba.guardar())
+                System.out.println("Datos archivados correctamente");
             else
-            System.out.println("Datos archivados correctamente");
+                System.out.println("Error al guardar en el archivo");            
         }
                         
         for(int i=0; i<almacenPrueba.cuenta();i++){
